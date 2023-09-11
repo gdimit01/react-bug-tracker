d945b571eaaa5abce977f6e12126b18532e20716
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-11 21:02:57 +0100

    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [newBugDescription, setNewBugDescription] = useState<string>("");
+  const [newBugPriority, setNewBugPriority] = useState<string>("Medium");
+  const [bugs, setBugs] = useState<
+    Array<{ title: string; description: string }>
+  >([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🐞 Bug Tracker</h1>
     </div>
   );
 }
