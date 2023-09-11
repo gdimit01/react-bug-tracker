26eb727512d83a03430d2cec8ad108325cdf2e81
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-11 21:11:13 +0100

    feat: "added add and delete"

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { IBug } from "./IBug";
 
 function App() {
   const [newBugDescription, setNewBugDescription] = useState<string>("");
   const [newBugPriority, setNewBugPriority] = useState<string>("Medium");
-  const [bugsList, setBugsList] = useState<
-    Array<{ title: string; description: string }>
-  >([]);
+  const [bugsList, setBugsList] = useState<IBug[]>([]);
+
+  const addNewBug = () => {};
+
+  const deleteBug = () => {};
+
   return (
     <div>
       <h1>🐞 Bug Tracker</h1>
