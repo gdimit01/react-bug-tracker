dd3118203f42ea8c62b62b3defbd8f2e4df77161
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-09-11 21:27:54 +0100

    feat: "added uuid"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-import { IBug } from "./IBug";
+import { BugPriority, IBug } from "./IBug";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const [newBugDescription, setNewBugDescription] = useState<string>("");
   const [newBugPriority, setNewBugPriority] = useState<string>("Medium");
   const [bugsList, setBugsList] = useState<IBug[]>([]);
 
-  const addNewBug = () => {};
+  const addNewBug = (event: FormEvent) => {
+    event.preventDefault();
+
+    const newBug: IBug = {
+      id: uuid(),
+      description: newBugDescription,
+      priority: newBugPriority as BugPriority,
+    };
+  };
 
   const deleteBug = () => {};
 
@@ -24,6 +33,17 @@ function App() {
           value={newBugDescription}
           onChange={(event) => setNewBugDescription(event.target.value)}
         />
+        <label htmlFor="newBugPriority">New bug priority: </label>
+        <select
+          id="newBugPriority"
+          value={newBugPriority}
+          onChange={(event) => setNewBugPriority(event.target.value)}
+        >
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+        <button type="submit">Add New Bug</button>
       </form>
     </div>
   );
